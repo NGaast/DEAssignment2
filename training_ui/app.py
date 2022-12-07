c48a558f5abf8a497a8ed06241caec3fd88ce72e
NGaastra <niels.gaastra@hotmail.com>
2022-12-07 13:50:03 +0100

    Added statistics ingestion

--- training_ui/app.py
+++ training_ui/app.py
@@ -20,16 +20,16 @@ app.secret_key = secret
 def training_ui():
     return render_template("training_template.html")  # this method is called of HTTP method is GET, e.g., when browsing the link
 
-@app.route('/upload_json_data', methods=['GET', 'POST'])
-def upload_data():
+@app.route('/upload_fixture_data', methods=['GET', 'POST'])
+def upload_fixtures_data():
     data_manager = DataManagement('de-2022-ng', 'data_de2022_ng')
     if request.method == "POST":
         # No file in request
-        if 'worldcup_data' not in request.files:
+        if 'fixture_data' not in request.files:
             print("No file in request", file=sys.stdout)
             return redirect(request.url)
         # Retrieve file
-        data_file = request.files['worldcup_data']
+        data_file = request.files['fixture_data']
         # No file
         if data_file.filename == '':
             print("No file selected", file=sys.stdout)
@@ -37,7 +37,7 @@ def upload_data():
             return redirect(request.url)
 
         json_format = json.load(data_file)
-        data_manager.store_json(json_format, "worldcup_json")
+        data_manager.store_json(json_format, "fixtures.json")
 
         print("Succesfully stored data", file=sys.stdout)
         sys.stdout.flush()
@@ -45,5 +45,32 @@ def upload_data():
 
         return redirect('/data_upload')
     return redirect('/data_upload')
+
+@app.route('/upload_statistics_data', methods=['GET', 'POST'])
+def upload_fixtures_data():
+    data_manager = DataManagement('de-2022-ng', 'data_de2022_ng')
+    if request.method == "POST":
+        # No file in request
+        if 'statistics_data' not in request.files:
+            print("No file in request", file=sys.stdout)
+            return redirect(request.url)
+        # Retrieve file
+        data_file = request.files['statistics_data']
+        # No file
+        if data_file.filename == '':
+            print("No file selected", file=sys.stdout)
+            sys.stdout.flush()
+            return redirect(request.url)
+
+        json_format = json.load(data_file)
+        data_manager.store_json(json_format, "statistics.json")
+
+        print("Succesfully stored data", file=sys.stdout)
+        sys.stdout.flush()
+
+
+        return redirect('/data_upload')
+    return redirect('/data_upload')
+    
 
 app.run(host='0.0.0.0', port=5000)
